0fb9abe3af7b337fc0311e582401265fc1300566
Cristian <i449915@student.fontys.nl>
2021-04-28 11:29:46 +0300

    Initial commit

---
+++ Workshop Management/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Management
+{
+    public abstract class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int pcn;
+        private Dictionary<Workshop, string> workshops;
+
+        public int PCN
+        {
+            get { return pcn; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"PCN cannot be zero or less");
+                }
+                pcn = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException($"Last Name cannot be empty");
+                }
+                lastName = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException($"First Name cannot be empty");
+                }
+                firstName = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int pcn)
+        {
+            this.workshops = new Dictionary<Workshop, string>();
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.PCN = pcn;
+        }
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} | {this.FirstName} {this.LastName}";
+        }
+    }
+}
---
+++ Workshop Management/Workshop.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop_Management
+{
+    public abstract class Workshop
+    {
+        private string title;
+        private List<Person> participants;
+        private int capacity;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException($"Title cannot be empty");
+                }
+                title = value;
+            }
+        }
+        public string Description { get; set; }
+        public int Capacity
+        {
+            get { return capacity; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Capacity cannot be 0 or less");
+                }
+                capacity = value;
+            }
+        }
+        public bool IsAvailable => this.participants.Count < this.Capacity;
+        public Workshop(string title, string description)
+        {
+            this.participants = new List<Person>();
+            this.Title = title;
+            this.Description = description;
+        }
+        public void AddParticipant(Person person)
+        {
+            if (this.participants.Contains(person))
+            {
+                throw new ArgumentException($"{person.FirstName} {person.LastName} is already in the workshop");
+            }
+            this.participants.Add(person);
+        }
+        public void RemoveParticipant(Person person)
+        {
+            if (!this.participants.Contains(person))
+            {
+                throw new ArgumentException($"{person.FirstName} {person.LastName} not found.");
+            }
+            this.participants.Remove(person);
+        }
+        public override string ToString()
+        {
+            return $"{this.Title} | Participants: {this.participants.Count} \n {this.Description}";
+        }
+    }
+}
